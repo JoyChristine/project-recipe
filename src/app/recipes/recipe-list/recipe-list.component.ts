50651a26916fcaf72f63511e6bf51932ce58ecbe
Joy Christine <joychristin2@gmail.com>
2022-04-08 22:00:58 +0300

    initial commit

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,11 +11,7 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('the name','the description',
     'https://images.unsplash.com/photo-1629740001727-f9ad15f720c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZGVsaWNpb3VzJTIwcGl6emF8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=700&q=60'),
-    new Recipe('the name','the description',
-    'https://images.unsplash.com/photo-1629740001727-f9ad15f720c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZGVsaWNpb3VzJTIwcGl6emF8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=700&q=60'),
-    new Recipe('the name','the description',
-    'https://images.unsplash.com/photo-1629740001727-f9ad15f720c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZGVsaWNpb3VzJTIwcGl6emF8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=700&q=60'),
-    
+   
 
   ]
   constructor() { }
